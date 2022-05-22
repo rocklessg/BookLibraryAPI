2500aba669fbe291c73e4d07da153b35c55544e4
rocklessg <abdulhafiz.suleiman@decagon.dev>
2022-05-22 05:10:17 +0100

    Generic CRUD operation Interfaces Implementation

--- BookLibrary.Domain/Entities/Category.cs
+++ BookLibrary.Domain/Entities/Category.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace BookLibrary.Domain.Entities
 {
-    public class Category
+    public class Category : BaseEntity
     {
+        public string Name { get; set; }
+
+        // Establishing one to many relationship
+        public ICollection<Book> Books { get; set; }
     }
 }
