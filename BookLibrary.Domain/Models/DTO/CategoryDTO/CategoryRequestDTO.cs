725a0eee3da3f0d32c200ceab75f315e88b17537
rocklessg <abdulhafiz.suleiman@decagon.dev>
2022-09-18 01:50:15 +0100

    Add book to Category Implementation wrap

--- BookLibrary.Domain/Models/DTO/CategoryDTO/CategoryRequestDTO.cs
+++ BookLibrary.Domain/Models/DTO/CategoryDTO/CategoryRequestDTO.cs
@@ -1,20 +1,30 @@
 ﻿using BookLibrary.Domain.Entities;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BookLibrary.Domain.Models.DTO.CategoryDTO
 {
-    public class CategoryRequestDTO : CategoryResponseDTO
+    public class CategoryRequestDTO
     {
-        public string Author { get; set; }
-        public string Publisher { get; set; }
-        public string PublishedDate { get; set; }
-        public string ISBN { get; set; }
-        public string ImageUrl { get; set; }
-        public string Description { get; set; }
-        public bool? IsFavorite { get; set; }
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public IEnumerable<Book> Books { get; set; }
+        //[Required]
+        //public ICollection<Book> Books { get; set; } = new List<Book>();
+        //public string Author { get; set; }
+        //public string Publisher { get; set; }
+        //public string PublishedDate { get; set; }
+        //public string ISBN { get; set; }
+        //public string ImageUrl { get; set; }
+        //public string Description { get; set; }
+        //public bool? IsFavorite { get; set; }
     }
 }
